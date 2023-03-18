24101f14d77a84482cf095bfd0be626ae08e6fd4
MashyBasker <basumaharshi10@gmail.com>
2023-03-18 11:04:35 +0530

    Modify game window GUI file

--- src/window.cpp
+++ src/window.cpp
@@ -1,13 +1,36 @@
 #include <ncurses.h>
-#include "window.h"
+#include "../lib/window.h"
+#include "../lib/constants.h"
 
 
 
-void GameWindow::create_game_window(int x, int y, int width, int height) {
+void GameWindow::create_game_window() {
 	// @params: x, y coordinates and the width, height from those coordinates
 	// @return: None
-	WINDOW *win;
 
-	attron(A_BOLD);
-	box()
+	initscr();
+	refresh();
+	noecho();
+	WINDOW *win;
+	win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, START_Y, START_X);
+	box(win, 0, 0);
+	wrefresh(win);
+	wgetch(win);
+
+	char ch;
+
+	// wait until user presses 'q'
+	while((ch = getch()) != 'q') {}
+
+	// close the
+	endwin();
+
+	
+
+}
+
+int main() {
+	GameWindow gm;
+	gm.create_game_window();
+	return 0;
 }
